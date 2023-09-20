be687ceb3ca97952d1393950baab8e43fc81593d
Ronaldyika <buhnyuyronald@gmail.com>
2023-09-20 23:47:11 +0100

    tired

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -6,9 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = {
     path('',user_views.index,name='index'),
     path('home/',user_views.home,name='home'),
-    path('registration/',user_views.RegisterUser,name='RegisterUser'),
+    path('registration/',user_views.RegisterUser,name='registration'),
 
     #==========this is for the signin and logout==============#
-    path('signin/',auth_views.LoginView.as_view(template_name = "login/login.html"),name='signin'),
+    path('loginuser/',auth_views.LoginView.as_view(template_name = "login/login.html"),name='loginuser'),
     path('logout/',auth_views.LogoutView.as_view(template_name = "login/logout.html"),name='logout'),
 }
