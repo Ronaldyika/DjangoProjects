cd2d9e04dc3b17ad386a57f6b5177c9e7d0bdec0
Ronaldyika <buhnyuyronald@gmail.com>
2023-09-27 14:21:45 +0100

    added the user gallery

--- MyPorfolio/MyPorfolio/settings.py
+++ MyPorfolio/MyPorfolio/settings.py
@@ -13,13 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-import environ
-
-env = environ.Env()
-
-environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6l#ayrt0&$v4ey+)l@#)=f$zyrej!_dz-29j2@^4w5@c=+iz76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
