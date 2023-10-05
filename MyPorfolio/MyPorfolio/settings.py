a782d8ad461d07bf7e84cdca37929186dbc54693
Ronaldyika <buhnyuyronald@gmail.com>
2023-10-05 19:35:24 +0100

    started deployment

--- MyPorfolio/MyPorfolio/settings.py
+++ MyPorfolio/MyPorfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6l#ayrt0&$v4ey+)l@#)=f$zyrej!_dz-29j2@^4w5@c=+iz76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'MyPorfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIR = [
-    BASE_DIR,'/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'porfolio_app/static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
