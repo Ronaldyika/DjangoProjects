32d13f115b2107c26e752af6ea702b038822caef
Ronaldyika <buhnyuyronald@gmail.com>
2023-10-06 04:24:29 +0100

    sr

--- MyPorfolio/porfolio_app/urls.py
+++ MyPorfolio/porfolio_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,re_path
 from . import views as user_view
 from django.contrib.staticfiles.views import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',user_view.home,name='index'),
@@ -9,6 +10,7 @@ urlpatterns = [
     path('Services/',user_view.Services,name='services'),
     path('Portfolio/',user_view.Porfolio,name='portfolio'),
     path('contact/',user_view.Contact,name='contact'),
-    re_path(r'^static/(?P<porfolio_app>.*)$',serve)
+    re_path(r'^static/(?P<MyPorfolio>.*)$',serve)
 
 ]
+urlpatterns += staticfiles_urlpatterns()
